752786d02aa10b0d06aec2148de07ade3d9bbac8
jeremyswann <jeremy@swanburger.com>
2019-06-12 15:00:21 -0700

    :zap: serverless config complete

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ process.env['PATH'] =
 
 exports.handler = function(event, context) {
 	var memStream = new MemoryStream()
-	wkhtmltopdf(event.html, event.options, function(code, signal) {
+	var html_utf8 = new Buffer(event.html_base64, 'base64').toString('utf8')
+	wkhtmltopdf(html_utf8, event.options, function(code, signal) {
 		context.done(null, { pdf_base64: memStream.read().toString('base64') })
 	}).pipe(memStream)
 }
